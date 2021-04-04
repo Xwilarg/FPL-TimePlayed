c1f8cdaa7ef4554485875373f9ef7b57c446784e
Xwilarg <xwilarg@yahoo.fr>
2021-04-04 20:05:56 +0200

    Add persistency

--- src/ext.ts
+++ src/ext.ts
@@ -1,7 +1,5 @@
 import * as flashpoint from 'flashpoint-launcher';
-import 'reflect-metadata';
 import { readFileSync, writeFileSync, existsSync } from 'fs';
-import { jsonObject, jsonMember, TypedJSON } from 'typedjson';
 import path = require('path');
 
 class GameLaunch {
@@ -14,11 +12,8 @@ class GameLaunch {
     }
 }
 
-@jsonObject
 class GameInfo {
-    @jsonMember
     totalTime: number;
-    @jsonMember
     gameName: string;
 
     constructor(gameName: string, currentTime: number) {
@@ -27,37 +22,34 @@ class GameInfo {
     }
 }
 
-let timers: Map<string, GameLaunch> = new Map();
-let times: Map<string, GameInfo> = new Map()
-const serializer = new TypedJSON(GameInfo);
+let timers: { [id: string] : GameLaunch; } = {};
+let times: { [id: string] : GameInfo; } = {};
 
 export function activate(context: flashpoint.ExtensionContext) {
 
-    if (existsSync("data/times.json")) {
-        times = JSON.parse(readFileSync(path.join(__dirname, "../data/times.json"), "utf8")).map((x: any) => {
-            return TypedJSON.parse(JSON.stringify(x), GameInfo);
-        });
+    if (existsSync(path.join(__dirname, "../data/times.json"))) {
+        times = JSON.parse(readFileSync(path.join(__dirname, "../data/times.json"), "utf8"));
     }
 
     flashpoint.games.onDidLaunchGame((game) => {
-        timers.set(game.id, new GameLaunch(game.title));
+        timers[game.id] = new GameLaunch(game.title);
     });
     flashpoint.services.onServiceRemove((process) => {
         if (process.id.startsWith('game.') && process.id.length > 5) {
             let closedId = process.id.substr(5);
-            let timer = timers.get(closedId);
+            let timer = timers[closedId];
             let diff = new Date().getTime() - timer.creaDate.getTime();
 
-            if (times.has(closedId)) {
-                times.get(closedId).totalTime += diff / 1000;
+            if (times[closedId] !== undefined) {
+                times[closedId].totalTime += diff / 1000;
             } else {
-                times.set(closedId, new GameInfo(timer.gameName, diff / 1000));
+                times[closedId] = new GameInfo(timer.gameName, diff / 1000);
             }
 
-            writeFileSync(path.join(__dirname, "../data/times.json"), "", "utf8");
+            writeFileSync(path.join(__dirname, "../data/times.json"), JSON.stringify(times), "utf8");
 
             delete timers[closedId];
-            flashpoint.log.info('Session of ' + timer.gameName + ' lasted ' + ((diff / 1000) / 60).toFixed(2) + ' minutes for a total of ' + (times.get(closedId).totalTime / 60).toFixed(2) + ' minutes');
+            flashpoint.log.info('Session of ' + timer.gameName + ' lasted ' + ((diff / 1000) / 60).toFixed(2) + ' minutes for a total of ' + (times[closedId].totalTime / 60).toFixed(2) + ' minutes');
         }
     });
 }
